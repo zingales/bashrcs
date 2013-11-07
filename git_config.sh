ddac7c8807673a2d515146211df09163837887b0
Giuseppe Zingales <zingales@mit.edu>
2013-11-07 14:14:45 -0500

    added even more colors to git

--- git_config.sh
+++ git_config.sh
@@ -1,3 +1,20 @@
 git config --global push.default upstream
 git config --global core.excludesfile ~/.gitignore_global
+
+#Git coloring
 git config --global color.ui true
+#---branch coloring
+git config --global color.branch.current "magenta"
+git config --global color.branch.local "yellow"
+git config --global color.branch.remote "green"
+#---diff coloring
+git config --global color.diff.meta "white"
+git config --global color.diff.frag "magenta bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+git config --global color.diff.whitespace "white reverse"
+#--- status coloring
+git config --global color.status.added "green"
+git config --global color.status.changed "yellow"
+git config --global color.status.untracked "cyan"
+git config --global color.status.branch "magenta"
