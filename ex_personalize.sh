f6467493b8ec2de9b5a963c42d713a698c183eb8
Giuseppe Zingales <zingales@mit.edu>
2014-07-28 17:08:08 -0400

    added a personlize script

---
+++ ex_personalize.sh
@@ -0,0 +1,4 @@
+#if you remove the ex_ this file will be run everytime you call install
+#useful for setting up some machine/user specific settings. I recoomend
+git config --global user.name "Name"
+git config --global user.email email 
