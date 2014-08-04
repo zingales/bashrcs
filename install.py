5f6faf348582a19336a295f1530b545ace02fda9
Giuseppe Zingales <zingales@mit.edu>
2014-08-04 19:55:27 -0400

    big reworking of the install script

--- install.py
+++ install.py
@@ -2,56 +2,121 @@ import os
 import time
 import shutil
 from os import walk
+import json
 
 #Global Vars
 homedir = os.path.expanduser("~")+"/"
-to_ignore = ['.DS_Store']
-toMove = [".gitconfig"]
+# to_ignore = ['.DS_Store']
+# toMove = [".gitconfig"]
 
-#genearte time as a string that can exist as a folder name
-temp_name = time.ctime()
-temp_name = temp_name.replace(" ", '_')
-temp_name = temp_name.replace(":", "_")
-old_rcs = "oldrcs/" + temp_name
+
+
+# modifyig this argument will have no affect, its overwritten in run
+to_ignore = []
+old_rcs = ""
+
 
 
 def guarantee_folder(folder):
-	if not os.path.exists(folder):
-		os.makedirs(folder)
+    if folder == homedir:
+        return
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-def save(filepath):
-	guarantee_folder(old_rcs)
-	if not os.path.exists(filepath):
-		return 
-	print "copying existant ", filepath, " to " +old_rcs
-	name = os.path.basename(filepath).replace(".", "")
-	shutil.copy(filepath, old_rcs+"/"+name)
+def save(path, isFolder=False):
+    guarantee_folder(old_rcs)
+    if not os.path.exists(path):
+        return 
+    print "copying existant ", path, " to " +old_rcs
+    name = os.path.basename(path)
 
-def install(filename, dest=homedir):
-	shutil.copy("files/"+filename, dest+"/."+filename)
+    to_ignore
+    if isFolder:
+        shutil.copytree(path, old_rcs+"/"+name, ignore=shutil.ignore_patterns(*to_ignore))
+    else:
+        name = name.replace(".", "")
+        shutil.copy(path, old_rcs+"/"+name)
+
+def install_file(filename, file_set):
+    if file_set.get("ignore", False) or filename in to_ignore:
+        return
+    folder = file_set.get("install_dir", "~")
+    folder = folder.replace("~", homedir)
+    guarantee_folder(folder)
+    dest = folder+file_set.get("install_name", "."+filename)
+    if file_set.get("save_original", True):
+        save(dest)
+    shutil.copy("files/"+filename, dest)
+
+def install_folder(foldername, folder_set):
+    if folder_set.get("ignore", False) or foldername in to_ignore:
+        return
+    folder = folder_set.get("install_dir", "~")
+    folder = folder.replace("~", homedir)
+
+    if folder_set.get("save_original", True):
+        save(folder, isFolder=True)
+    guarantee_folder(folder)
+
+    src_folder = "files/"+foldername
+    for filename in os.listdir(src_folder):
+        shutil.copy(src_folder+"/"+filename, folder+"/"+filename)
 
 def run():
-	toInstallraw = []
-	for (dirpath, dirnames, filenames) in walk("./files"):
-	    if (dirpath == "./files"):
-        	toInstallraw.extend(filenames)
+    settings = {}
+    files_to_install = []
+    folders_to_install = []
 
-	for x in to_ignore:
-		if x in toInstallraw:
-			toInstallraw.remove(x)
+    #generate time as a string that can exist as a folder name
+    temp_name = time.ctime()
+    temp_name = temp_name.replace(" ", '_')
+    temp_name = temp_name.replace(":", "_")
+    global old_rcs
+    old_rcs = "oldrcs/" + temp_name
 
-	toCheck = ["."+x for x in toInstallraw]
+    if not os.path.exists(homedir+".bash_profile"):
+        print "Your really gonna need a bash_profile"
 
-	for filename in os.listdir(homedir):
-		if filename in toCheck:
-			toMove.append(filename)
-		
 
-	for fileToMove in toMove:
-		save(homedir+fileToMove)
+    #load file settings
+    with open("install_settings.json", 'r') as jsonFile:
+        settings = json.loads(jsonFile.read())
 
-	for fileToInstall in toInstallraw:
-		install(fileToInstall)
+    global to_ignore
+    to_ignore = settings.get("to_ignore", [".DS_Store"])
+
+    #saving specific files not in ./files
+    for original_file in settings.get("to_save", []):
+        save(original_file)
+    
+    #load all files and directores in ./files
+    for (dirpath, dirnames, filenames) in walk("./files"):
+        if (dirpath == "./files"):
+            files_to_install = filenames
+            folders_to_install = dirnames
+
+    for filename in files_to_install:
+        install_file(filename, settings.get(filename, {}))
+
+    for foldername in folders_to_install:
+        install_folder(foldername, settings.get(foldername, {}))
+
+    # for x in to_ignore:
+    #   if x in toInstallraw:
+    #       toInstallraw.remove(x)
+
+    # toCheck = ["."+x for x in toInstallraw]
+
+    # for filename in os.listdir(homedir):
+    #   if filename in toCheck:
+    #       toMove.append(filename)
+        
+
+    # for fileToMove in toMove:
+    #   save(homedir+fileToMove)
+
+    # for fileToInstall in toInstallraw:
+    #   install(fileToInstall)
 
 if __name__ == "__main__":
-	run()
+    run()
