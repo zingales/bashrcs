0cfcbd2588821bd6f775a8501b3cae1589197091
Giuseppe <zingales@mit.edu>
2015-02-27 12:19:23 -0500

    added copy pasta in ttmux+vim when on mac

--- ex_personalize.sh
+++
@@ -1,4 +0,0 @@
-#if you remove the ex_ this file will be run everytime you call install
-#useful for setting up some machine/user specific settings. I recoomend
-git config --global user.name "Name"
-git config --global user.email email 
